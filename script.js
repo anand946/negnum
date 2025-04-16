9f713fbf340bb7a29245823437f5536736d01e7c
anand946 <munhchuluunjambalsanjid6@gmail.com>
2025-04-16 19:38:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let yesButtonSize = 1;
 
     yesButton.addEventListener('click', () => {
-        responseMessage.textContent = "🫶";
+        responseMessage.textContent = "🥰";
         yesButton.style.transform = 'scale(1)'; // Ensure size is normal when clicking "Yes"
     });
 
